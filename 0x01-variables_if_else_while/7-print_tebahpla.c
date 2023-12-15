490519850a71d622c90750badf81dac049b835e8
calvincec <calyndemo16@gmail.com>
2023-12-15 23:48:04 +0300

    added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: A program that prints the lowercase alphabet in reverse, followed by a new line.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: A program that prints all the numbers of base 16 in lowercase, followed by a new line.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+
+	for (n = 97; n <= 102; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
