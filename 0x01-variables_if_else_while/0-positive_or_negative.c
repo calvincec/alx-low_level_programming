90004b786d357979424ed921d1e9c8a4e47cd300
calvincec <calyndemo16@gmail.com>
2023-12-15 22:00:50 +0300

    positive or negative number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* Betty style doc for function main goes there */
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    /* Calculate the last digit of the number stored in the variable n */
+    int last_digit = n % 10;
+    if (last_digit < 0)
+        last_digit = last_digit * -1;
+
+    printf("Last digit of %d is %d. ", n, last_digit);
+    if (last_digit > 5)
+        printf("And is greater than 5");
+    else if (last_digit == 0)
+        printf("And is 0");
+    else if (last_digit < 6 && last_digit != 0)
+        printf("And is less than 6 and not 0");
+
+    return (0);
+}
