b43b27b165000de6522392e48d02446f1278d584
calvincec <calyndemo16@gmail.com>
2023-12-16 00:23:13 +0300

    coded the optional tasks

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			for (k = j; k < 10; k++)
+			{
+				for (l = k; l < 10; l++)
+				{
+					if (i == k && j == l)
+						continue;
+
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
