153a08e324ffe7be0fab780ff873e51de54add60
calvincec <calyndemo16@gmail.com>
2023-12-15 22:44:16 +0300

    added quiz 1 and fixed some bugs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,23 +6,18 @@
 int main(void)
 {
     int n;
-    int last_digit;
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    /* Calculate the last digit of the number stored in the variable n */
-    last_digit = n % 10;
-    if (last_digit < 0)
-        last_digit = last_digit * -1;
-
-    printf("Last digit of %d is %d. ", n, last_digit);
-    if (last_digit > 5)
-        printf("And is greater than 5");
-    else if (last_digit == 0)
-        printf("And is 0");
-    else if (last_digit < 6 && last_digit != 0)
-        printf("And is less than 6 and not 0");
+    /* prints the random number and states if it 0, negative or positive */
+    printf("%d is ", n);
+    if (n > 0)
+        printf("positive\n");
+    else if (n == 0)
+        printf("zero\n");
+    else if (n < 0)
+        printf("negative\n");
 
     return (0);
 }
