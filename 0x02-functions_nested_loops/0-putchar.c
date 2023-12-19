7c960d26bddfef672e5d65e1ae95e8f64415a434
calvincec <calyndemo16@gmail.com>
2023-12-19 18:49:17 +0300

    followed strict betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-    char *c = "_putchar";
-    int i;
+	char *c = "_putchar";
+	int i;
 
-    for (i = 0; i < 8; i++)
-        _putchar(c[i]);
-    _putchar('\n');
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
