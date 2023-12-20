f1aa491bf8fae094889bacbf46ef7793efd763e8
calvincec <calyndemo16@gmail.com>
2023-12-20 20:47:37 +0300

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-    int n;
-    int sum = 0;
+	int n;
+	int sum = 0;
 
-    for (n = 0; n < 1024; n++)
-    {
-        if ((n % 3 == 0) || (n % 5 == 0))
-        {
-            sum += n;
-        }
-    }
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			sum += n;
+		}
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return 0;
+	return (0);
 }
