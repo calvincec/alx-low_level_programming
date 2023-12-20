862e3cb0961b8ed33d2b4f404eb324dfd34706e1
calvincec <calyndemo16@gmail.com>
2023-12-20 21:21:04 +0300

    task 102 corretced

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,24 +7,24 @@
  */
 int main(void)
 {
-    long i = 0;
-    long j = 1;
-    int n = 0;
-    long fib;
+	long i = 0;
+	long j = 1;
+	int n = 0;
+	long fib;
 
-    while (n < 50)
-    {
-        fib = j + i;
+	while (n < 50)
+	{
+		fib = j + i;
 
-        if (n != 49)
-            printf("%ld, ", fib);
-        else
-            printf("%ld\n", fib);
+		if (n != 49)
+			printf("%ld, ", fib);
+		else
+			printf("%ld\n", fib);
 
-        i = j;
-        j = fib;
-        n += 1;
-    }
+		i = j;
+		j = fib;
+		n += 1;
+	}
 
-    return (0);
+	return (0);
 }
